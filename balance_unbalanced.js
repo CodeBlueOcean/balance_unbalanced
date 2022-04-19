73218051f0c8037ec59fc2221ec40bbc30a32d14
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-04-18 20:49:09 -0500

    Add files via upload

---
+++ balance_unbalanced.js
@@ -0,0 +1,15 @@
+// Balanced vs Unbalanced 
+// Lookup O(n)
+// Insert O(n)
+// Delete O(n)
+
+// Lookup O
+
+// Pros and Cons
+// Binary Search Trees
+// Better than O(n)
+// Ordered 
+// Flexible Size
+
+// No O(1) operations
+
